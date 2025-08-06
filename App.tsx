cb7268aecaf5a1932864735ef2dda38d0a45230d
Your Name <s3979760@rmit.edu.vn>
2025-08-06 16:10:53 +0700

    update

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { createClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+function App() {
+  const [instruments, setInstruments] = useState([]);
+  useEffect(() => {
+    getInstruments();
+  }, []);
+  async function getInstruments() {
+    const { data } = await supabase.from("instruments").select();
+    setInstruments(data);
+  }
+  return (
+    <ul>
+      {instruments.map((instrument) => (
+        <li key={instrument.name}>{instrument.name}</li>
+      ))}
+    </ul>
+  );
+}
+export default App;
+
